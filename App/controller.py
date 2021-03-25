b7dda014d04e590ca8ad65f6a1d2fef38b44123a
Ernesto J Duarte <e.duartem@uniandes.edu.co>
2021-03-25 17:03:01 -0500

    large

--- App/controller.py
+++ App/controller.py
@@ -66,7 +66,7 @@ def loadData(catalog):
 def loadVideos(catalog):
 
     # Videos:
-    videosfile = cf.data_dir + 'videos-5pct.csv'
+    videosfile = cf.data_dir + 'videos-large.csv'
     input_file = csv.DictReader(open(videosfile, encoding='utf-8'))
     for video in input_file:
         model.addVideo(catalog, video)
